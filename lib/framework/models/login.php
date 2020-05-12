d4426be349b402fbc7288de0918cb6b6346c4947
s4r14k <sarikraf@gmail.com>
2020-05-12 15:57:08 +0200

    remove image

--- lib/framework/models/login.php
+++ lib/framework/models/login.php
@@ -9,10 +9,8 @@ class Login {
 		$email = filter_var($email, FILTER_SANITIZE_STRING);
 
 		if ($req = $my_db->prepare('
-				SELECT u.id, u.email, u.pass, u.nom, u.prenom, u.role, g.image1, c.is_confirm
+				SELECT u.id, u.email, u.pass, u.nom, u.prenom, u.role
 				FROM utilisateur u
-				LEFT JOIN gallerie_images g ON g.id_user = u.id
-				LEFT JOIN confirmation c ON c.id_user = u.id 
 				WHERE u.email = :email
 				LIMIT 1 
 			')) {
