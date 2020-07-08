7ed6e2e2a850a633506a290fea5f6521a20c2823
s4r14k <sarikraf@gmail.com>
2020-07-08 18:15:03 +0200

    Gestion message

--- lib/framework/gestionmail/message.php
+++ lib/framework/gestionmail/message.php
@@ -1,25 +1,22 @@
 <?php
-namespace Framework\GestionMail;
+namespace framework\gestionmail;
 
 class Message {
-	private $_to = "alphagroup@gmail.com";
-	private $_from;
-	private $_name;
-	private $_email;
-	private $_body;
 
-	function __construct ($name_, $from_, $body_) {
-		$this->_name = 'Website Contact From:' . $name_;
-		$this->_from = $from_;
-		$this->_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: {$name_}\n\nEmail: {$from_}\n\nMessage:\n{$body_}";
-	}
-
-	function Envoyer () {
-		$headers = "From: noreply@yourdomain.com\n";
-		$headers .= "Reply-To: {$this->_to}";
-		mail($this->_to, $this->_name, $this->_body, $headers);
-		return true;
-	}
-
-
+    static function set($id_to, $id_from, $body, $reason, $my_db) {
+        if ($stmt = $my_db->prepare('
+			  INSERT INTO messages
+			  SET id_to = :id_to, id_from = :id_from, body = :body, reason = :reason, date_send = NOW()
+			')) {
+			    $stmt->execute(array(
+			        'id_to' => $id_to,
+			        'id_from' => $id_from,
+			        'body' => $body,
+			        'reason' => $reason
+			    ));
+	  } else {
+		  echo "ERROR";
+	  }
+	    $stmt->closeCursor();
+    }
 }
